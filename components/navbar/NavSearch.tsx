28b7b747c575a7c509005fabf4a6e46ddf3f78a0
taraneh <taraneh.m5257@gmail.com>
2024-12-12 20:44:17 +0330

    add searchbox functionalities to search a property

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,11 +1,40 @@
-import { Input } from '../ui/input';
+"use client";
+import { Input } from "../ui/input";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useDebounce, useDebouncedCallback } from "use-debounce";
+import { useState, useEffect } from "react";
 
 function NavSearch() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+  const [search, setSearch] = useState<string>(
+    searchParams.get("search")?.toString() || ""
+  );
+
+  const handleSearch = useDebouncedCallback((value: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (value) {
+      params.set("search", value);
+    } else {
+      params.delete("search");
+    }
+    router.replace(`${pathname}?${params.toString()}`);
+  }, 500);
+
+  useEffect(() => {
+    if (!searchParams.get("search")) setSearch("");
+  }, [searchParams.get("search")]);
   return (
     <Input
-      type='search'
-      placeholder='find a property...'
-      className='max-w-xs dark:bg-muted '
+      type="search"
+      placeholder="find a property..."
+      className="max-w-xs dark:bg-muted "
+      value={search}
+      onChange={(e) => {
+        setSearch(e.target.value);
+        handleSearch(e.target.value);
+      }}
     />
   );
 }
