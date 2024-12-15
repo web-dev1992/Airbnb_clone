83622b1b753788ffcd2378e44877e32f3125efcc
taraneh <taraneh.m5257@gmail.com>
2024-12-15 20:08:07 +0330

    add bookingCalendar component

---
+++ components/properties/BookingCalendar.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { useState } from "react";
+import { Calendar } from "@/components/ui/calendar";
+import { DateRange } from "react-day-picker";
+
+ function BookingCalendar() {
+  const currentDate = new Date();
+  const defaultSelected: DateRange = {
+    from: undefined,
+    to: undefined,
+  };
+  const [range, setRange] = useState<DateRange | undefined>(defaultSelected);
+
+  return (
+    <Calendar
+      id="test"
+      mode="range"
+      defaultMonth={currentDate}
+      selected={range}
+      onSelect={setRange}
+    />
+  );
+}
+
+
+export default BookingCalendar;
