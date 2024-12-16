27259328db4a646a39da691bd12240a91e54f7e1
taraneh <taraneh.m5257@gmail.com>
2024-12-16 19:07:29 +0330

    redirect to home page at search time

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,12 +1,11 @@
 "use client";
 import { Input } from "../ui/input";
-import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
 import {  useDebouncedCallback } from "use-debounce";
 import { useState, useEffect } from "react";
 
 function NavSearch() {
   const searchParams = useSearchParams();
-  const pathname = usePathname();
   const router = useRouter();
   const [search, setSearch] = useState<string>(
     searchParams.get("search")?.toString() || ""
@@ -19,7 +18,7 @@ function NavSearch() {
     } else {
       params.delete("search");
     }
-    router.replace(`${pathname}?${params.toString()}`);
+    router.replace(`/?${params.toString()}`);
   }, 500);
 
   useEffect(() => {
