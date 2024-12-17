fa642def28c154d6d3b73fa77be7135696bd4335
taraneh <taraneh.m5257@gmail.com>
2024-12-17 17:14:47 +0330

    add all components and functionalities for booking a vacation destination

---
+++ components/booking/BookingForm.tsx
@@ -0,0 +1,36 @@
+import { calculateTotals } from "@/utils/calculateTotals";
+import { useProperty } from "@/utils/store"
+import { Card, CardTitle } from "../ui/card";
+import { Separator } from "@radix-ui/react-dropdown-menu";
+import { formatCurrency } from "@/utils/format";
+
+function BookingForm() {
+  const { range, price} =useProperty((state)=>state)
+  const checkIn= range?.from as Date;
+  const checkOut= range?.to as Date;
+  const { totalNights, subTotal, orderTotal, cleaning, service, tax }=calculateTotals({checkIn, checkOut, price})
+  return (
+    <Card className="p-8 mb-4">
+      <CardTitle className="mb-8">Summary </CardTitle>
+      <FormRow label={`$${price} x ${totalNights} nights`} amount={subTotal} />
+      <FormRow label="Cleaning Fee" amount={cleaning} />
+      <FormRow label="Service Fee" amount={service} />
+      <FormRow label="Tax" amount={tax} />
+      <Separator className="mt-4" />
+      <CardTitle className="mt-8">
+        <FormRow label="Booking Total" amount={orderTotal} />
+      </CardTitle>
+    </Card>
+  );
+}
+
+function FormRow({ label, amount }: { label: string; amount: number }) {
+  return (
+    <p className="flex justify-between text-sm mb-2">
+      <span>{label}</span>
+      <span>{formatCurrency(amount)}</span>
+    </p>
+  );
+}
+
+export default BookingForm
--- utils/actions.ts
+++ utils/actions.ts
@@ -13,6 +13,7 @@ import {
 import { createClerkClient } from "@clerk/nextjs/server"; // Import the create function
 import { revalidatePath } from "next/cache";
 import { uploadImage } from "./supabase";
+import { calculateTotals } from "./calculateTotals";
 
 // Instantiate Clerk client
 const clerkClient = createClerkClient({
@@ -390,4 +391,43 @@ export const findExistingReview = async (
       propertyId: propertyId,
     },
   });
+};
+
+export const createBookingAction = async (prevState: {
+  propertyId: string;
+  checkIn: Date;
+  checkOut: Date;
+}) => {
+  const user = await getAuthUser();
+  const { propertyId, checkIn, checkOut } = prevState;
+  const property = await db.property.findUnique({
+    where: {
+      id: propertyId,
+    },
+    select: {
+      price: true,
+    },
+  });
+  if (!property) return { message: " property not found!!" };
+  const { orderTotal, totalNights } = calculateTotals({
+    checkIn,
+    checkOut,
+    price: property.price,
+  });
+  try {
+    const booking = await db.booking.create({
+      data: {
+        checkIn,
+        checkOut,
+        orderTotal,
+        totalNights,
+        profileId: user.id,
+        propertyId,
+      },
+    });
+
+  } catch (error) {
+    return renderError(error);
+  }
+  redirect('/bookings')
 };
