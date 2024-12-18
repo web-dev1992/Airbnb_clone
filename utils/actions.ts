c39024052034673c0785877235f54ebf0cc97559
taraneh <taraneh.m5257@gmail.com>
2024-12-18 18:59:52 +0330

    add reservation page and its acctions and functionalities

--- utils/actions.ts
+++ utils/actions.ts
@@ -576,3 +576,26 @@ export const updatePropertyImageAction = async (
   revalidatePath(`/rentals/${propertyId}/edit`);
   return { message: "Property image updated successfully!" };
 };
+
+export const fetchReservations = async () => {
+  const user = await getAuthUser();
+  const reservations = await db.booking.findMany({
+    where: {
+      property: {
+        profileId: user.id,
+      },
+    },
+    orderBy: { createdAt: "desc" },
+    include: {
+      property: {
+        select: {
+          id: true,
+          name: true,
+          price: true,
+          country: true,
+        },
+      },
+    },
+  });
+  return reservations;
+};
